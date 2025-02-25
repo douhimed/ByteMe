90a527d9d602113c6f3f986391f46f3e822cdca5
mdouhi <mdouhi@sqli.com>
2025-02-25 16:26:47 +0100

    readme & java file

---
+++ App.java
@@ -0,0 +1,8 @@
+public abstract class App {
+    private static void hello(String message) {
+        System.out.println("Hello " + message);
+    }
+    public static void main(String[] args) {
+        hello("Mohammed");
+    }    
+}
